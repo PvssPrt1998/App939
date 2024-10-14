c8eec504cbe8edb9776acf1486140d98ac15f3c5
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-10-14 13:31:54 +0500

    Initial Commit

---
+++ App939/App939App.swift
@@ -0,0 +1,17 @@
+//
+//  App939App.swift
+//  App939
+//
+//  Created by Николай Щербаков on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct App939App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
